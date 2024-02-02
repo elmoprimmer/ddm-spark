587e48793792540805a2775543bfb3277a9ff631
Elmo Primmer <elmo.primmer@gmail.com>
2024-02-02 08:55:27 +0100

    tried to optimise with partitions + added documentation

--- src/main/scala/de/ddm/Sindy.scala
+++ src/main/scala/de/ddm/Sindy.scala
@@ -1,5 +1,6 @@
 package de.ddm
 
+import org.apache.spark.sql.catalyst.dsl.expressions.StringToAttributeConversionHelper
 import org.apache.spark.sql.{Dataset, Row, SparkSession}
 import org.apache.spark.sql.functions.{col, collect_set}
 
@@ -18,23 +19,31 @@ object Sindy {
 
 
   def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
-    val data = inputs.map(input => readData(input, spark))
+    // Calculate a good number of partitions based on the cluster configuration
+    val coresPerExecutor = spark.conf.getOption("spark.executor.cores").getOrElse("4").toInt
+    val numExecutors = spark.conf.getOption("spark.executor.instances").getOrElse("2").toInt
+    val totalCores = coresPerExecutor * numExecutors
 
+    // Read and repartition data for each input file
+    val data = inputs.map(input => readData(input, spark).repartition(totalCores*3))
+
+    // Extract column sets by mapping each DataFrame to its columns and transforming into RDDs
     val columnSets = data
       .flatMap(df => df.columns.map(column => df.select(column)
-        .rdd.map(row => (row(0), Set[String](column))))) //maybe there is a better way to get a set
-      .reduce(_ union _).reduceByKey(_ ++ _).values.distinct()
+        .rdd.map(row => (row(0), Set[String](column))))) // Union all RDDs and reduce by key to merge sets for the same key (column value)
+      .reduce(_ union _).coalesce(totalCores*2).reduceByKey(_ ++ _).values.distinct()  // Reduce number o partitions for final calculations, hopefully for efficiency
+    // e.g. Combines tuples like (valueInA, Set("A")) for all unique values in column "A"
 
-
+    // Generate possible inclusion lists by pairing each column with the rest of the columns in its set
     val possibleInclusionsList = columnSets.flatMap(set => set.map(column => (column, set.filterNot(_ == column))))
 
-    val inclusions = possibleInclusionsList.reduceByKey(_.intersect(_)).filter(_._2.nonEmpty)
+    // Find inclusions by intersecting column sets for each column across all values
+    val inclusions = possibleInclusionsList.reduceByKey(_.intersect(_))
 
-    val inclusionsSorted = inclusions.sortByKey().collect()
+    // Sort and prepare for printing
+    val inclusionsSorted = inclusions.filter(_._2.nonEmpty).sortByKey().collect() // .collect executes pipeline
 
     inclusionsSorted.foreach(column => print(column._1 + " < " + column._2.mkString(", ")+ "\n"))
-
-    //TODO: comments
 
   }
 }
